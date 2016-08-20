a73c79f5884ee975f7467c9f3b88ac4ca5d3c98c
Blake <ambsneakysnack@gmail.com>
2016-08-19 21:52:32 -0400

    small progress

--- Day11/Day11Part1.py
+++ Day11/Day11Part1.py
@@ -1,2 +1,7 @@
 pattern = 'hxbxwxba'
 
+while(True):
+    # Need to check and see if all conditions are met
+    # First condition: contains one "straight" of 3 letters
+    # Ex: 'abc' or 'fgh'
+    for i in range(len(pattern)):
